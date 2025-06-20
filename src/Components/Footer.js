f242a9250d69a4eec8248538bf3ae6eec8fbcbf4
Aarti Gupta <aartigupta24sep@gmail.com>
2025-06-20 12:10:59 +0530

    deleted subscribe letter

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../Styles/Footer.css";
-import SubscribeNewsletter from "./SubscribeNewsletter";
 import { Link } from "react-router-dom";
 
 function Footer() {
@@ -18,7 +17,6 @@ function Footer() {
             </p>
           </div>
 
-          <SubscribeNewsletter />
         </div>
 
         <div className="ft-list">
